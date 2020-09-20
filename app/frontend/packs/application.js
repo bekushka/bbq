5df8a30a2bc73d0b2061b6233c0a5dde3b64492f
Bekzod Tursunov <amishopogolik@gmail.com>
2020-09-21 01:00:56 +0500

    initial commit

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap/dist/js/bootstrap'
+import '../styles/applications.scss'
+const images = require.context('../images', true)
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
