4f7ae7cd32408a2e0456709c29480b67f5558489
Bekzod Tursunov <amishopogolik@gmail.com>
2020-09-28 23:35:34 +0500

    fixed issue with layouts

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module BBQ
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Moscow'
+    config.time_zone = 'Tashkent'
 
     config.i18n.default_locale = :ru
     config.i18n.locale = :ru
