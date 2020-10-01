ca64b1b5415da4a9d9d892911dae31acfa0ccae8
Bekzod Tursunov <amishopogolik@gmail.com>
2020-10-02 00:23:48 +0500

    delete event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,10 @@ class ApplicationController < ActionController::Base
 
   # Вспомогательный метод, возвращает true, если текущий залогиненный юзер
   # может править указанное событие
-  def current_user_can_edit?(model)
+  def current_user_can_edit?(event)
     user_signed_in? && (
-    model.user == current_user ||
-        (model.try(:event).present? && model.event.user == current_user)
+    event.user == current_user ||
+        (event.try(:event).present? && event.event.user == current_user)
     )
   end
 end
