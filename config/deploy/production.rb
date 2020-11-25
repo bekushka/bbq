b4a7265a33b20f438c0474ea5076d5293ca9b5ac
Bekzod Tursunov <amishopogolik@gmail.com>
2020-11-26 02:11:07 +0500

    Cap ready to install

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,8 +9,8 @@
 
 server "party-hub.ru", user: "deploy", roles: %w{app db web resque_worker}
 
-set :resque_environment_task, true
-set :workers, { "#{fetch(:application)}*" => 1 }
+#set :resque_environment_task, true
+#set :workers, { "#{fetch(:application)}*" => 1 }
 # role-based syntax
 # ==================
 
