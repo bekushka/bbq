53718327c055ca39766a841271164b888ba69dac
Bekzod Tursunov <amishopogolik@gmail.com>
2020-12-14 01:32:34 +0500

    added omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -77,8 +77,6 @@ ActiveRecord::Schema.define(version: 2020_12_06_185015) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "events"
-  add_foreign_key "comments", "users"
   add_foreign_key "events", "users"
   add_foreign_key "photos", "events"
   add_foreign_key "photos", "users"
